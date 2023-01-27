5e68ea28578ececf859f85a163321da5401f37bb
Ferdousmohammed <ferdousmohammed29@gmail.com>
2023-01-28 01:18:06 +0600

    loop&function commited

---
+++ function.js
@@ -0,0 +1,83 @@
+/*
+function sendLove(){
+
+    console.log('jAAN O BABY');
+    console.log('sona moyna pakhii') ;
+    console.log('kobe tumi asbe buke');
+    console.log('basbe vlo');
+ }
+ sendLove ();  */
+
+function love (num){
+    var double = num * 2;
+    console.log(double);
+
+
+}
+
+love(5);
+love(10);
+
+
+
+function like(lol) {
+    var int =  lol * 3;
+    console.log(int);
+}
+
+
+
+like(10);
+
+
+function moon (sun){
+    var harunSir = sun * 3;
+    console.log(harunSir);
+}
+
+
+moon (10);
+moon (15);
+
+
+function jhankarBhai (Karina){
+        var kenosha = Karina / 5;
+        return kenosha;
+        
+}
+
+ var fst = jhankarBhai(100);
+ var snd =  jhankarBhai(200);
+ var rd  = jhankarBhai(300);
+ var total = ( fst + snd + rd  );
+ console.log(total);
+
+
+function  prova (nirob){
+    var eva = nirob * 3;
+    console.log(eva);
+}
+
+prova(10);
+prova(20);
+
+
+
+
+function  prova (nirob){
+    var eva = nirob * 2 ;
+return eva;
+
+}
+
+var first = prova(10);
+var second = prova(20);
+var total = first + second;
+console.log(total);
+
+function add ( num1, num2){
+    var res = num1 + num2;
+    return res
+}
+var sum = add(num1+num2);
+console.log(sum);
---
+++ while.js
@@ -0,0 +1,37 @@
+var numBer = 10;
+
+while (numBer <=29 ) {
+
+console.log(numBer);
+numBer ++ ;
+}
+
+var kola = 5;
+
+ while (kola < 15){
+
+console.log(kola);
+kola ++;
+
+
+ }
+
+ var love = 3 ;
+ while (love <= 10){
+    console.log(love);
+    love ++;
+ }
+
+
+ for (var i=0; i<9; i++){
+     console.log(i);
+
+
+ }
+
+ for (var i=0; i<=10; i++){
+console.log(i);
+ }
+
+
+
